3792b0e57b98cc6802581492b1569e626e95b659
hendikaseptio <hendikaseptio11@gmail.com>
2025-10-12 16:15:05 +0700

    feature: minify js

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,15 +1,1 @@
-const clientSwiper = new Swiper(".clientSwiper", {
-    slidesPerView: 4, // jumlah logo yang tampil sekaligus
-    spaceBetween: 10,
-    loop: true,
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: false, // tetap autoplay meskipun digeser
-    },
-    grabCursor: true, // bisa drag pakai mouse
-    breakpoints: {
-        320: { slidesPerView: 2, spaceBetween: 20 },
-        640: { slidesPerView: 3, spaceBetween: 25 },
-        1024: { slidesPerView: 5, spaceBetween: 30 },
-    },
-});
+const clientSwiper=new Swiper(".clientSwiper",{slidesPerView:4,spaceBetween:10,loop:!0,autoplay:{delay:2e3,disableOnInteraction:!1},grabCursor:!0,breakpoints:{320:{slidesPerView:2,spaceBetween:20},640:{slidesPerView:3,spaceBetween:25},1024:{slidesPerView:5,spaceBetween:30}}});
